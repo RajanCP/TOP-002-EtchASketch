33624ff50183fcc508d5ce77d3f6b2e75a722dfb
RajanCP <rjnptl2000@gmail.com>
2025-08-11 22:57:00 +0100

    JS - Added logic to limit range and started to create function to create empty grid canvas

--- script.js
+++ script.js
@@ -4,9 +4,29 @@ const gridContainer = document.getElementById("gridContainer");
 
 resizeBtn.addEventListener("click", promptfn)
 
-console.log("Hello World");
-
 function promptfn() {
   const gridSize = prompt("Enter the number of squares per side:");
   console.log(`User entered: ${gridSize}`);
+
+    if (gridSize >= 1 && gridSize <= 100) {
+      createGrid(gridSize); 
+    } else {
+      alert("Please enter a number between 1 and 100.");
+    }
+  
+}
+
+function createGrid(){
+
+gridContainer.innerHTML = "";
+
+const squareSize = 960 / gridSize;
+
+for (let i = 0; i < gridSize*gridSize; i++) {
+     const square = document.createElement("div");
+     square.classList.add("grid-square");
+}
+
+
+
 }
